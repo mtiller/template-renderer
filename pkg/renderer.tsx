5017eb79fd7a72b6ce61a8d6ead947203a00c848
Michael Tiller <michael.tiller@gmail.com>
2025-03-08 21:00:33 -0500

    doc: demonstration app working

--- pkg/renderer.tsx
+++ pkg/renderer.tsx
@@ -3,9 +3,10 @@ import { useTemplate } from "./template";
 export interface TemplateRendererProps {
   content: string;
   values: Record<string, any>;
+  style?: React.CSSProperties;
 }
 
 export const TemplateRenderer = (props: TemplateRendererProps) => {
   const rendered = useTemplate(props.content, props.values);
-  return <div>{rendered}</div>;
+  return <div style={props.style}>{rendered}</div>;
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import "./App.css";
 import { TemplateRenderer } from "../pkg";
+import { useEffect, useState } from "react";
+
+const initialTemplate = "This is a sample...x = {{ x }}";
+
+const initialValue = { x: 5 };
 
 function App() {
+  const [template, setTemplate] = useState<string>(initialTemplate);
+  const [valueText, setValueText] = useState<string>(
+    JSON.stringify(initialValue)
+  );
+  const [value, setValue] = useState<any>(initialValue);
+
+  useEffect(() => {
+    try {
+      const val = JSON.parse(valueText);
+      setValue(val);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [valueText]);
   return (
-    <TemplateRenderer
-      content="This is a sample...x = {{ x }}"
-      values={{ x: 5 }}
-    ></TemplateRenderer>
+    <div style={{ height: "100vh", width: "100vw", display: "flex" }}>
+      <div style={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+        <textarea
+          style={{ flexGrow: 1 }}
+          onChange={(ev) => setTemplate(ev.target.value)}
+        >
+          {template}
+        </textarea>
+        <textarea
+          style={{ flexGrow: 1 }}
+          onChange={(ev) => setValueText(ev.target.value)}
+        >
+          {valueText}
+        </textarea>
+      </div>
+      <TemplateRenderer
+        style={{ flexGrow: 1 }}
+        content={template}
+        values={value}
+      ></TemplateRenderer>
+    </div>
   );
 }
 
