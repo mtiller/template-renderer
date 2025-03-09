34548a0b1353ca1193404e0fbf22425736617a99
Michael Tiller <michael.tiller@gmail.com>
2025-03-09 11:57:59 -0400

    refactor: initial config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "https://mtiller.github.io/template-renderer/",
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["pkg"] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "pkg/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
 });
