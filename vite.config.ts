020e1b4b48f5b01fd35ff23bc4917fd577f19c06
Michael Tiller <michael.tiller@gmail.com>
2025-03-09 11:21:38 -0400

    ci: tweak asset includes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/mtiller/template-renderer",
+  base: "https://github.com/mtiller/template-renderer/",
+  assetsInclude: ["**/*.js", "**/*.css"],
   plugins: [react()],
 });
