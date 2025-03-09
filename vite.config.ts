f20e1e729d37886bcfaa6960ebbb84356f623b83
Michael Tiller <michael.tiller@gmail.com>
2025-03-09 11:29:54 -0400

    ci: had wrong base...ugh

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/mtiller/template-renderer/",
-  assetsInclude: ["**/*.js", "**/*.css"],
+  base: "https://mtiller.github.io/template-renderer/",
   plugins: [react()],
 });
